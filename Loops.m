fe234e6434ab773fa5eb0898993bd0db41a262c5
ethanho3 <ethan176h@gmail.com>
2025-02-27 16:40:53 -0500

    Update Loops.m

--- Loops.m
+++ Loops.m
@@ -14,3 +14,11 @@ row_sums = zeros(1, size(A,1));
 for i = 1:size(A,1)
     row_sums(i) = sum(A(i,:));
 end
+
+A = [3 5 7 2 9]
+for i = 1:length(A)
+    if A(i) < 5
+        continue
+    end
+    disp(['value ' num2str(A(i)) ' is >= 5'])
+end
